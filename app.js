1d4927ae026b0f8d9f137ebb88b08bbc4d27e711
Himanshu Swain <himanshuswain117@gmail.com>
2023-04-24 05:47:32 +0000

    Update 4 files

---
+++ app.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("button");
+
+function darkThemeToggler() {
+  const container = document.getElementById("container");
+  container.classList.toggle("dark-theme");
+}
+Footer
